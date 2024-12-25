c4950bfeda21cef55704a08fc4c06bff087c1d0f
Rohit Deshmukh <112280707+drohit02@users.noreply.github.com>
2024-12-25 11:48:47 +0530

    Update SpringbootJenkinsApplication.java

--- springboot-jenkins/src/main/java/com/example/jenkinsdemo/SpringbootJenkinsApplication.java
+++ springboot-jenkins/src/main/java/com/example/jenkinsdemo/SpringbootJenkinsApplication.java
@@ -2,12 +2,18 @@ package com.example.jenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class SpringbootJenkinsApplication {
+public class SpringbootJenkinsApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootJenkinsApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootJenkinsApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJenkinsApplication.class, args);
+    }
 }
